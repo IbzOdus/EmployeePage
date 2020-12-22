746dbef08dbe3081584fdc23e5ca014faafe6850
Ibz <ayomikun8219@gmail.com>
2020-12-22 04:22:25 +0000

    Finish initial interests section template

--- src/components/views/InterestsSection.jsx
+++ src/components/views/InterestsSection.jsx
@@ -6,18 +6,21 @@ import { colors } from '../../constants';
 
 const Container = styled.div`
     display: flex;
-    justify-content: space-between;
+    justify-content: space-around;
     align-items: center;
-    width: 100%;
+    max-width: 1400px;
 `;
 
 const ActionImage = styled.img`
-    box-shadow: 0px 0px 30px 15px;
+    box-shadow: 0px 0px 10px 5px;
+    height: 700px;
 `; 
 
 const InterestsText = styled.div`
-    flex: 1;
-    /* align-items: center */
+    text-align: center;
+    width: 50%;
+    min-width: 200px;
+    padding: 20px;
 `;
 
 const InterestsSection = () => {
@@ -26,7 +29,9 @@ const InterestsSection = () => {
             <PageHeading>Interests</PageHeading>
             <Container>
                 <ActionImage src={interestsImage} />
-                <InterestsText>Txt</InterestsText>
+                <InterestsText>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </InterestsText>
             </Container>
         </PageWrapper>
     )
