c4b39c1135cab1e42738ee9cd1087f7a3c8652bc
Ibz <ayomikun8219@gmail.com>
2020-12-22 03:23:28 +0000

    Add image to interests section

--- src/components/views/InterestsSection.jsx
+++ src/components/views/InterestsSection.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
-import backgroundImage from '../../images/Code Background.png';
+import interestsImage from '../../images/falaq-lazuardi-2Ul7W3d5pkE-unsplash.jpg';
 import { PageWrapper, PageHeading } from '../pages/Common';
 import { colors } from '../../constants';
+
+const Container = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+`;
+
+const ActionImage = styled.img`
+    box-shadow: 0px 0px 30px 15px;
+`; 
+
+const InterestsText = styled.div`
+    flex: 1;
+    /* align-items: center */
+`;
 
 const InterestsSection = () => {
     return (
         <PageWrapper id="interests" color={colors.primary}>
             <PageHeading>Interests</PageHeading>
-            <br />
-                <br />
-                <span style={{fontSize: '40px', textAlign: 'center'}}>I'm Person Name<br/>a full stack developer of x years.<br/>I attended y university and graduated with<br/>some degree</span>
-                <br />
-                <span style={{fontSize: '40px', textAlign: 'center'}}>I'm Person Name<br/>a full stack developer of x years.<br/>I attended y university and graduated with<br/>some degree</span>
-                <br />
-                <span style={{fontSize: '40px', textAlign: 'center'}}>I'm Person Name<br/>a full stack developer of x years.<br/>I attended y university and graduated with<br/>some degree</span>
+            <Container>
+                <ActionImage src={interestsImage} />
+                <InterestsText>Txt</InterestsText>
+            </Container>
         </PageWrapper>
     )
 };
