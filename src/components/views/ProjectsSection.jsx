a667eeea6ed80bf080089346e631f897f045f81a
Ibz <ayomikun8219@gmail.com>
2020-12-22 06:28:56 +0000

    Finish projects section layout

--- src/components/views/ProjectsSection.jsx
+++ src/components/views/ProjectsSection.jsx
@@ -7,6 +7,39 @@ import projectThumbnail from '../../images/app-icon-3364869_1280.png';
 
 const Container = styled.div`
     display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+`;
+
+const CardsContainer = styled.div`
+    display: flex;
+    flex: 1;
+    justify-content: space-evenly;
+`;
+
+const NavArrow = styled.span`
+    color: ${colors.primaryLight};
+    font-size: 200px;
+
+    &:hover {
+        cursor: pointer;
+        color: ${colors.user};
+    }
+`;
+
+const NavIndicator = styled.div`
+    display: flex;
+    justify-content: space-around;
+    width: 10%;
+    margin-top: 30px;
+`;
+
+const NavIndicatorPoint = styled.span`
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    background-color: ${props => (props.active && colors.user) || '#FFFFFF'};
 `;
 
 export default function ProjectsSection() {
@@ -14,12 +47,32 @@ export default function ProjectsSection() {
         <PageWrapper id="projects" backgroundColor={colors.primary} color={colors.primary}>
             <PageHeading color={'#FFFFFF'}>Projects</PageHeading>
             <Container>
-                <ProjectCard
-                    name={'Project Name Name'}
-                    summary={'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}
-                    thumbnail={projectThumbnail}
-                    points={["Cross-Platform application", "Has feature X", "Y million downloads", "Z thousand active users"]}/>
+                <NavArrow>{'<'}</NavArrow>
+                <CardsContainer>  
+                    <ProjectCard
+                        name={'Project Name Name'}
+                        summary={'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}
+                        thumbnail={projectThumbnail}
+                        points={["Cross-Platform application", "Has feature X", "Y million downloads", "Z thousand active users"]}/>
+                    <ProjectCard
+                        name={'Project Name Name'}
+                        summary={'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}
+                        thumbnail={projectThumbnail}
+                        points={["Cross-Platform application", "Has feature X", "Y million downloads", "Z thousand active users"]}/>
+                    <ProjectCard
+                        name={'Project Name Name'}
+                        summary={'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}
+                        thumbnail={projectThumbnail}
+                        points={["Cross-Platform application", "Has feature X", "Y million downloads", "Z thousand active users"]}/>
+                </CardsContainer>
+                <NavArrow>{'>'}</NavArrow>
             </Container>
+            <NavIndicator>
+                <NavIndicatorPoint active/>
+                <NavIndicatorPoint active />
+                <NavIndicatorPoint active />
+                <NavIndicatorPoint />
+            </NavIndicator>
         </PageWrapper>
     )
 };
