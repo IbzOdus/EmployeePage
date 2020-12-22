f82c78694e51921508995b40b5fd71fe15ddfb32
Ibz <ayomikun8219@gmail.com>
2020-12-22 08:01:46 +0000

    Create basic loader and not found page

---
+++ src/components/Loader.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+import { colors } from '../constants';
+
+const LoadingDiv = styled.div`
+    border: 10px solid #f3f3f3;
+    border-top: 10px solid ${colors.primary};
+    border-radius: 50%;
+    width: 100px;
+    height: 100px;
+    margin-top: 50px;
+    animation: spin 2s linear infinite;
+
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+}
+`;
+
+export default function Loader() {
+    return <LoadingDiv />
+}
---
+++ src/components/pages/NotFoundPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    margin-top: 50px;
+    text-align: center;
+`;
+
+export default function NotFoundPage() {
+    return (
+        <Container>
+            Sorry, We couldn't find the page you were looking for
+        </Container>
+    )
+}
